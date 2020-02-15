1601bd27b98452f66916e5b2088fadf850c3d5be
marsel1323 <amn132313@gmail.com>
2020-02-15 18:33:53 +0300

    added eslint and started adding redux with saga

--- src/App.js
+++ src/App.js
@@ -2,29 +2,38 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="header">Header</header>
-        <div className="table">
-            <table>
-                <thead>
-                <tr>
-                    <th>thhh</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>tdddd</td>
-                </tr>
-                </tbody>
-            </table>
+    return (
+        <div className="App">
+            <header className="header">Header</header>
+            <div className="table">
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Header 1</th>
+                        <th>Header 2</th>
+                        <th>Header 3</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>body 1</td>
+                        <td>body 2</td>
+                        <td>body 3</td>
+                    </tr>
+                    <tr>
+                        <td>body 1</td>
+                        <td>body 2</td>
+                        <td>body 3</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <br/>
+            <div className="details">
+                Details
+            </div>
         </div>
-        <br/>
-        <div className="details">
-            Details
-        </div>
-    </div>
-  );
+    );
 }
 
 export default App;
