bf4ee9660678cf7619a42413ae2bcd229a082141
marsel1323 <amn132313@gmail.com>
2020-03-12 12:19:04 +0300

    started implemented in class components with life cicles

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,75 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import { createStructuredSelector } from 'reselect';
-import { connect } from 'react-redux';
-import { selectFillTextDatas } from './redux/fillText/fillText.selectors';
-import { fetchFillTextDataStart } from './redux/fillText/fillText.actions';
+import { fetchData } from './api/fillText';
 
-function App({ fillTextData, fetchFillTextData }) {
-  useEffect(() => {
-    fetchFillTextData();
-  }, [fetchFillTextData]);
-  return (
-    <div className="App">
-      <header className="header">Header</header>
-      <div className="table">
-        <table>
-          <thead>
-            <tr>
-              <th>***</th>
-              <th>id</th>
-              <th>firstName</th>
-              <th>lastName</th>
-              <th>email</th>
-              <th>phone</th>
-            </tr>
-          </thead>
-          <tbody>
-            {fillTextData.map((data) => (
-              <tr key={data.id}>
-                <td>*</td>
-                <td>{data.id}</td>
-                <td>{data.firstName}</td>
-                <td>{data.lastName}</td>
-                <td>{data.email}</td>
-                <td>{data.phone}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fillTextData: null,
+    };
+  }
+
+  async componentDidMount() {
+    const data = await this.fetchFillTextData();
+    this.setState({
+      fillTextData: data,
+    });
+  }
+
+  fetchFillTextData() {
+    return fetchData(32);
+  }
+
+  render() {
+    const { fillTextData } = this.state;
+    return (
+      <div className="App">
+        <header className="header">Header</header>
+
+        {
+          fillTextData
+            ? (
+              <div className="table">
+                <table>
+                  <thead>
+                    <tr>
+                      <th>***</th>
+                      <th>id</th>
+                      <th>firstName</th>
+                      <th>lastName</th>
+                      <th>email</th>
+                      <th>phone</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {fillTextData.map((data) => (
+                      <tr key={data.id}>
+                        <td>*</td>
+                        <td>{data.id}</td>
+                        <td>{data.firstName}</td>
+                        <td>{data.lastName}</td>
+                        <td>{data.email}</td>
+                        <td>{data.phone}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            )
+            : (
+              <div>Loading</div>
+            )
+        }
+
+        <br />
+        <div className="details">
+          Details
+        </div>
       </div>
-      <br />
-      <div className="details">
-        Details
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
-const mapStateToProps = createStructuredSelector({
-  fillTextData: selectFillTextDatas,
-});
-
-
-const mapDispatchToProps = (dispatch) => ({
-  fetchFillTextData: () => dispatch(fetchFillTextDataStart()),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
 
-import { persistor, store } from './redux/store';
-
-ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
